74b24624dc8d679bce194121d69a39d68dec5a24
Rajsinh123 <rajsinhmandawale15@gmail.com>
2024-07-09 11:48:32 +0530

    Add project files.

---
+++ SalaryCalculationUsingWebFrom/Form1.Designer.cs
@@ -0,0 +1,286 @@
+﻿
+namespace SalaryCalculationUsingWebFrom
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtSal = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.chketf = new System.Windows.Forms.CheckBox();
+            this.chkepf = new System.Windows.Forms.CheckBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtetf = new System.Windows.Forms.TextBox();
+            this.txtepf = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtgsal = new System.Windows.Forms.TextBox();
+            this.txtallo = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.txtnsal = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(166, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Verdana", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(220, 21);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(304, 23);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "EmployeeSalaryCalculation";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(131, 79);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(81, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "EmployeeName";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(131, 125);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(36, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Salary";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(239, 79);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(100, 20);
+            this.txtName.TabIndex = 4;
+            // 
+            // txtSal
+            // 
+            this.txtSal.Location = new System.Drawing.Point(239, 125);
+            this.txtSal.Name = "txtSal";
+            this.txtSal.Size = new System.Drawing.Size(100, 20);
+            this.txtSal.TabIndex = 5;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.txtnsal);
+            this.groupBox1.Controls.Add(this.label9);
+            this.groupBox1.Controls.Add(this.txtallo);
+            this.groupBox1.Controls.Add(this.label8);
+            this.groupBox1.Controls.Add(this.txtgsal);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.txtepf);
+            this.groupBox1.Controls.Add(this.txtetf);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.chkepf);
+            this.groupBox1.Controls.Add(this.chketf);
+            this.groupBox1.Location = new System.Drawing.Point(57, 174);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(503, 283);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Calculation";
+            // 
+            // chketf
+            // 
+            this.chketf.AutoSize = true;
+            this.chketf.Location = new System.Drawing.Point(6, 34);
+            this.chketf.Name = "chketf";
+            this.chketf.Size = new System.Drawing.Size(46, 17);
+            this.chketf.TabIndex = 0;
+            this.chketf.Text = "ETF";
+            this.chketf.UseVisualStyleBackColor = true;
+            // 
+            // chkepf
+            // 
+            this.chkepf.AutoSize = true;
+            this.chkepf.Location = new System.Drawing.Point(6, 78);
+            this.chkepf.Name = "chkepf";
+            this.chkepf.Size = new System.Drawing.Size(46, 17);
+            this.chkepf.TabIndex = 1;
+            this.chkepf.Text = "EPF";
+            this.chkepf.UseVisualStyleBackColor = true;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(93, 38);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(63, 13);
+            this.label5.TabIndex = 3;
+            this.label5.Text = "ETFAmount";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(92, 82);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(63, 13);
+            this.label6.TabIndex = 4;
+            this.label6.Text = "EPFAmount";
+            // 
+            // txtetf
+            // 
+            this.txtetf.Location = new System.Drawing.Point(167, 38);
+            this.txtetf.Name = "txtetf";
+            this.txtetf.Size = new System.Drawing.Size(100, 20);
+            this.txtetf.TabIndex = 5;
+            // 
+            // txtepf
+            // 
+            this.txtepf.Location = new System.Drawing.Point(167, 82);
+            this.txtepf.Name = "txtepf";
+            this.txtepf.Size = new System.Drawing.Size(100, 20);
+            this.txtepf.TabIndex = 6;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(6, 152);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(63, 13);
+            this.label7.TabIndex = 7;
+            this.label7.Text = "GrossSalary";
+            // 
+            // txtgsal
+            // 
+            this.txtgsal.Location = new System.Drawing.Point(77, 152);
+            this.txtgsal.Name = "txtgsal";
+            this.txtgsal.Size = new System.Drawing.Size(100, 20);
+            this.txtgsal.TabIndex = 8;
+            // 
+            // txtallo
+            // 
+            this.txtallo.Location = new System.Drawing.Point(328, 152);
+            this.txtallo.Name = "txtallo";
+            this.txtallo.Size = new System.Drawing.Size(85, 20);
+            this.txtallo.TabIndex = 10;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(263, 152);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(56, 13);
+            this.label8.TabIndex = 9;
+            this.label8.Text = "Allowence";
+            // 
+            // txtnsal
+            // 
+            this.txtnsal.Location = new System.Drawing.Point(328, 203);
+            this.txtnsal.Name = "txtnsal";
+            this.txtnsal.Size = new System.Drawing.Size(100, 20);
+            this.txtnsal.TabIndex = 12;
+            this.txtnsal.TextChanged += new System.EventHandler(this.textBox7_TextChanged);
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(254, 206);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(53, 13);
+            this.label9.TabIndex = 11;
+            this.label9.Text = "NetSalary";
+            this.label9.Click += new System.EventHandler(this.label9_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(294, 241);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 13;
+            this.button1.Text = "Cal";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.txtSal);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "EmployeeSalary ";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtSal;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtnsal;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox txtallo;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox txtgsal;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtepf;
+        private System.Windows.Forms.TextBox txtetf;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.CheckBox chkepf;
+        private System.Windows.Forms.CheckBox chketf;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ SalaryCalculationUsingWebFrom/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SalaryCalculationUsingWebFrom
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+       private void textBox7_TextChanged(object sender, EventArgs e)
+       {
+
+       }
+
+        private void label9_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        Double sal, etf, epf, etfamount, epfamount, gsal, allow, nsal;
+        
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            sal = Double.Parse(txtSal.Text);
+            if (chketf.Checked == true)
+            {
+                etfamount = sal * 10 / 100;
+            }
+            if (chkepf.Checked == true)
+            {
+                epfamount = sal * 5 / 100;
+            }
+           
+            txtetf.Text = etfamount.ToString();
+            txtepf.Text = epfamount.ToString();
+           
+            gsal = sal - epfamount + etfamount;
+            txtgsal.Text = gsal.ToString();
+            if (sal > 50000)
+            {
+                allow = 2000;
+            }
+            else if (sal > 30000)
+            {
+                allow = 1000;
+            }
+            else
+            {
+                allow = 0;
+            }
+            txtallo.Text = allow.ToString();
+            nsal = gsal + allow;
+            txtnsal.Text = nsal.ToString();
+        }
+    }
+}
